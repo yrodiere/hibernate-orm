de2d91160397e4fbe1029799703704fadba020e0
Yoann Rodière <yoann@hibernate.org>
2025-01-02 14:47:31 +0100

    HHH-18976 Deprecate ArrayHelper#newInstance in favor of the standard Array#newInstance

--- hibernate-core/src/main/java/org/hibernate/internal/util/collections/ArrayHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/collections/ArrayHelper.java
@@ -484,6 +484,10 @@ public final class ArrayHelper {
 		}
 	}
 
+	/**
+	 * @deprecated Use {@link Array#newInstance(Class, int)} instead.
+	 */
+	@Deprecated
 	@SuppressWarnings("unchecked")
 	@AllowReflection
 	public static <T> T[] newInstance(Class<T> elementType, int length) {
